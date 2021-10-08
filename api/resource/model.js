c6c54847f684667fdfbfd0ee9fd53f0d1244463e
Christina Yun <poncheeyun@yahoo.com>
2021-10-08 10:27:35 -0700

    Finished resources get and post

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,12 +1,21 @@
-const db = require('./../../data/dbConfig');
+const db = require("./../../data/dbConfig");
 
-function getResources(){
-    return db('resources').select('*')
-};
+function getResources() {
+  return db("resources").select("*");
+}
 
-function createResource(newResource){};
+function getResourceById(resource_id) {
+  return db("resources").select("*").where("resource_id", resource_id).first();
+}
+
+async function createResource(newResource) {
+  const newId = await db("resources").insert(newResource);
+
+  return getResourceById(newId);
+}
 
 module.exports = {
-    getResources,
-    createResource
-}
+  getResources,
+  getResourceById,
+  createResource,
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,18 +1,32 @@
-const express = require('express');
-const Resources = require('./model');
+const express = require("express");
+const Resources = require("./model");
 
 const router = express.Router();
- 
+
 // [GET] /api/resources
-router.get('/', (req, res, next) => {
-    Resources.getResources()
-        .then(resources => {
-            res.json(resources)
-        })
-        .catch(next)
+router.get("/", (req, res, next) => {
+  Resources.getResources()
+    .then((resources) => {
+      res.json(resources);
+    })
+    .catch(next);
+});
+
+//[GET] /api/resources/:resource_id
+router.get("/:resource_id", (req, res, next) => {
+  Resources.getResourceById(req.params.resource_id)
+    .then((resource) => {
+      res.json(resource);
+    })
+    .catch(next);
 });
 
 // [POST] /api/resources
-// Example of response body: {"resource_id":1,"resource_name":"foo","resource_description":null}
-router.get('/', (req, res, next) => {});
+router.post("/", (req, res, next) => {
+  Resources.createResource(req.body)
+    .then((newResource) => {
+      res.status(201).json(newResource);
+    })
+    .catch(next);
+});
 module.exports = router;
