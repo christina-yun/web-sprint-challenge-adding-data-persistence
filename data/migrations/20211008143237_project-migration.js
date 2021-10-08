bbed315d008e965b2c29b656a9807fd9efbc6dab
Christina Yun <poncheeyun@yahoo.com>
2021-10-08 08:13:05 -0700

    Migration successful and the tables are formatted correctly

---
+++ data/migrations/20211008143237_project-migration.js
@@ -0,0 +1,53 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 128).notNullable()
+        table.string('project_description', 250)
+        table.integer('project_completed').notNullable().defaultTo(0)
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_description', 250).notNullable()
+        table.string('task_notes', 128)
+        table.integer('task_completed').notNullable().defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name', 128).unique().notNullable()
+        table.string('resource_description', 128)
+    })
+    .createTable('project_resources', table => {
+        table.increments('project_resource_id')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
+};
